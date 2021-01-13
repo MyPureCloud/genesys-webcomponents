3ea2445e247eb68f907a2ab13ffc4f5bde0f577d
Daragh King <daragh.king@genesys.com>
2021-01-13 14:52:00 +0000

    test(beta-components): improved tests in some beta components
    improved tests in some beta components
    
    COMUI-366

--- src/components/beta/gux-tabs/tests/gux-tabs.spec.ts
+++ src/components/beta/gux-tabs/tests/gux-tabs.spec.ts
@@ -1,22 +1,68 @@
-import { newSpecPage } from '@stencil/core/testing';
+import { newSpecPage, SpecPage } from '@stencil/core/testing';
+
 import { GuxTabs } from '../gux-tabs';
+import { GuxTab } from '../gux-tab/gux-tab';
+import { GuxTabDropdownOption } from '../gux-tab-dropdown-option/gux-tab-dropdown-option';
+
+const components = [GuxTabs, GuxTab, GuxTabDropdownOption];
+const language = 'en';
 
 describe('gux-tabs', () => {
-  let component: GuxTabs;
+  let page: SpecPage;
 
   beforeEach(async () => {
-    const page = await newSpecPage({
-      components: [GuxTabs],
-      html: `<gux-tabs-beta></gux-tabs-beta>`,
-      language: 'en'
-    });
+    page = await newSpecPage({
+      components,
+      html: `
+        <gux-tabs-beta id="interactive">
+          <gux-tab tab-id="1" tab-icon-name="ic-locked">
+            <span slot="title"> Hello World </span>
+            <span slot="dropdown-options">
+              <gux-tab-dropdown-option
+                option-id="1"
+                icon-name="ic-pencil"
+                onclick="notify(event)"
+              >
+                Edit
+              </gux-tab-dropdown-option>
+              <gux-tab-dropdown-option
+                option-id="2"
+                icon-name="ic-clone"
+                onclick="notify(event)"
+              >
+                Clone
+              </gux-tab-dropdown-option>
+              <gux-tab-dropdown-option
+                option-id="3"
+                icon-name="ic-share"
+                onclick="notify(event)"
+              >
+                Share
+              </gux-tab-dropdown-option>
+              <gux-tab-dropdown-option
+                option-id="4"
+                icon-name="ic-download"
+                onclick="notify(event)"
+              >
+                Download
+              </gux-tab-dropdown-option>
+            </span>
+          </gux-tab>
 
-    component = page.rootInstance;
+          <gux-tab tab-id="2" tab-icon-name="ic-locked">
+            <span slot="title"> Hello World 2 </span>
+          </gux-tab>
+        </gux-tabs-beta>
+      `,
+      language
+    });
   });
 
-  describe('Class Logic', () => {
-    it('should pass', () => {
-      expect(true).toBeTruthy();
-    });
+  it('should build', async () => {
+    expect(page.rootInstance).toBeInstanceOf(GuxTabs);
+  });
+
+  it('should render', async () => {
+    expect(page.root).toMatchSnapshot();
   });
 });
