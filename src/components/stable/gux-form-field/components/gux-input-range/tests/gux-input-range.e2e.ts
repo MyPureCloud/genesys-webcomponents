51fe63e97dba867918491999d2f1130bbdf44ac5
Daragh King <daragh.king@genesys.com>
2021-01-13 14:52:00 +0000

    test(beta-components): improved tests in some beta components
    improved tests in some beta components
    
    COMUI-366

--- src/components/stable/gux-form-field/components/gux-input-range/tests/gux-input-range.e2e.ts
+++ src/components/stable/gux-form-field/components/gux-input-range/tests/gux-input-range.e2e.ts
@@ -8,7 +8,11 @@ describe('gux-input-radio', () => {
       </gux-input-radio>
     `;
     const page = await newE2EPage({ html });
+<<<<<<< HEAD:src/components/stable/gux-form-field/components/gux-input-range/tests/gux-input-range.e2e.ts
     const element = await page.find('gux-input-radio');
+=======
+    const element = await page.find('gux-input-radio-beta');
+>>>>>>> 0ba4b54... test(beta-components): improved tests in some beta components:src/components/beta/form/gux-form-field/components/gux-input-range-beta/tests/gux-input-range-beta.e2e.ts
 
     expect(element).toHaveClass('hydrated');
   });
