33e861fe6761427bc6040944561ba2dc6e2d0368
Daragh King <daragh.king@genesys.com>
2021-01-21 11:52:11 +0000

    feat(components): made components replaced by gux-form-field legacy
    made components replaced by gux-form-field legacy
    
    BREAKING CHANGE: made components replaced by gux-form-field legacy
    
    COMUI-345

--- src/components/stable/gux-form-field/components/gux-input-color/components/gux-color-select/tests/gux-input-color-select.spec.ts
+++ src/components/stable/gux-form-field/components/gux-input-color/components/gux-color-select/tests/gux-input-color-select.spec.ts
@@ -1,16 +1,16 @@
 import { newSpecPage } from '@stencil/core/testing';
 import { GuxInputColorOption } from '../../gux-input-color-option/gux-input-color-option';
-import { GuxInputColorSelect } from '../gux-input-color-select';
+import { GuxColorSelect } from '../gux-color-select';
 
-const components = [GuxInputColorSelect, GuxInputColorOption];
+const components = [GuxColorSelect, GuxInputColorOption];
 const language = 'en';
 
-describe('gux-input-color-select', () => {
+describe('gux-color-select', () => {
   it('should build', async () => {
-    const html = `<gux-input-color-select><input slot="input" type="color" value="#75A8FF"></gux-input-color-select>`;
+    const html = `<gux-color-select><input slot="input" type="color" value="#75A8FF"></gux-color-select>`;
     const page = await newSpecPage({ components, html, language });
     const component = page.rootInstance;
 
-    expect(component).toBeInstanceOf(GuxInputColorSelect);
+    expect(component).toBeInstanceOf(GuxColorSelect);
   });
 });
