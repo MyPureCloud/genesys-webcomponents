341a9e6c082fa0ee9e11e52a2cd571a40c1b03f8
Daragh King <daragh.king@genesys.com>
2021-01-21 11:52:11 +0000

    feat(components): made components replaced by gux-form-field legacy
    made components replaced by gux-form-field legacy
    
    BREAKING CHANGE: made components replaced by gux-form-field legacy
    
    COMUI-345

--- src/components/stable/gux-form-field/components/gux-input-color/components/gux-color-select/tests/gux-input-color-select.e2e.ts
+++ src/components/stable/gux-form-field/components/gux-input-color/components/gux-color-select/tests/gux-input-color-select.e2e.ts
@@ -1,11 +1,11 @@
 import { newE2EPage } from '@stencil/core/testing';
 
-describe('gux-input-color-select', () => {
+describe('gux-color-select', () => {
   it('renders', async () => {
     const html =
-      '<gux-input-color-select><input slot="input" type="color" value="#75A8FF"></gux-input-color-select>';
+      '<gux-color-select><input slot="input" type="color" value="#75A8FF"></gux-color-select>';
     const page = await newE2EPage({ html });
-    const element = await page.find('gux-input-color-select');
+    const element = await page.find('gux-color-select');
 
     expect(element).toHaveClass('hydrated');
   });
