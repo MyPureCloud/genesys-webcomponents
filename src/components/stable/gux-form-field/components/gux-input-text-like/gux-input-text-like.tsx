811516ae2bd3b6ae97c6d18bf312f0f24908d3f3
Daragh King <daragh.king@genesys.com>
2021-01-26 10:05:42 +0000

    feat(form-field): added clear button to text like fields
    added clear button to text like fields
    
    COMUI-392

--- src/components/stable/gux-form-field/components/gux-input-text-like/gux-input-text-like.tsx
+++ src/components/stable/gux-form-field/components/gux-input-text-like/gux-input-text-like.tsx
@@ -1,4 +1,9 @@
-import { Component, h, JSX } from '@stencil/core';
+import { Component, Element, h, JSX, Prop, State } from '@stencil/core';
+
+import { buildI18nForComponent, GetI18nValue } from '../../../../../i18n';
+import setInputValue from '../../../../../utils/dom/set-input-value';
+
+import modalComponentResources from './i18n/en.json';
 
 /**
  * @slot input - Required slot for input[type="radio"]
@@ -9,7 +14,67 @@ import { Component, h, JSX } from '@stencil/core';
   tag: 'gux-input-text-like'
 })
 export class GuxInputTextLike {
+  private input: HTMLInputElement;
+  private getI18nValue: GetI18nValue;
+
+  @Element()
+  private root: HTMLElement;
+
+  @Prop()
+  clearable: boolean;
+
+  @State()
+  private hasContent: boolean = false;
+
+  private clearInput(): void {
+    setInputValue(this.input, '', true);
+  }
+
+  private setHasContent(): void {
+    this.hasContent = Boolean(this.input.value);
+  }
+
+  private renderClearButton(): JSX.Element {
+    if (this.clearable && this.hasContent) {
+      return (
+        <button
+          class="gux-clear-button"
+          type="button"
+          title={this.getI18nValue('clear')}
+          onClick={this.clearInput.bind(this)}
+        >
+          <gux-icon
+            screenreaderText={this.getI18nValue('clear')}
+            iconName="ic-close"
+          ></gux-icon>
+        </button>
+      );
+    }
+
+    return null;
+  }
+
+  async componentWillLoad(): Promise<void> {
+    this.getI18nValue = await buildI18nForComponent(
+      this.root,
+      modalComponentResources
+    );
+
+    this.input = this.root.querySelector(
+      'input[slot="input"], select[slot="input"]'
+    );
+
+    this.input.addEventListener('input', () => {
+      this.setHasContent();
+    });
+  }
+
   render(): JSX.Element {
-    return <slot name="input" />;
+    return (
+      <div class="gux-input-container">
+        <slot name="input" />
+        {this.renderClearButton()}
+      </div>
+    );
   }
 }
